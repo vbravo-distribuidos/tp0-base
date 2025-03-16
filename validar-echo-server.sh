0543401037579d943123a8d779af6cfef0e740d2
Victor Bravo <vbravo@fi.uba.ar>
2025-03-16 19:13:54 -0300

    EJ3 - Llamo a busybox desde validar-echo-server

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,13 +1,7 @@
-# Test echo server
-
-MESSAGE='test'
-SERVER_IP=server
-SERVER_PORT=12345
-
-RESPONSE=$(echo "$MESSAGE" | nc $SERVER_IP $SERVER_PORT)
-
-if [ "$RESPONSE" == "$MESSAGE" ]; then
-    echo 'action: test_echo_server | result: success'
-else
-    echo 'action: test_echo_server | result: fail'
-fi
+docker pull busybox
+docker run -it --rm --network tp0_testing_net busybox sh -c \
+"if echo test | nc server 12345 | grep -q test; then \
+    echo 'action: test_echo_server | result: success'; \
+ else \
+    echo 'action: test_echo_server | result: fail' \
+; fi"
