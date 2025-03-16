fec05eab3d13236f41710d178e6c7496bf1b4861
Victor Bravo <vbravo@fi.uba.ar>
2025-03-16 19:23:14 -0300

    EJ3 - Redicciono stdout y stderr a dev null al hacer docker pull

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,4 +1,4 @@
-docker pull busybox
+docker pull busybox 1> /dev/null 2> /dev/null
 docker run -it --rm --network tp0_testing_net busybox sh -c \
 "if echo test | nc server 12345 | grep -q test; then \
     echo 'action: test_echo_server | result: success'; \
