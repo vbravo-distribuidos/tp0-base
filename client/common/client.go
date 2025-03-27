dfcf49221f475de41f46730946de87cfbaa6c074
Victor Bravo <vbravo@fi.uba.ar>
2025-03-27 00:54:30 -0300

    EJ8 - Sleep en clientes para flushear ultimos mensajes

--- client/common/client.go
+++ client/common/client.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/7574-sistemas-distribuidos/docker-compose-init/client/common/modelo"
 	"github.com/7574-sistemas-distribuidos/docker-compose-init/client/common/protocolo"
@@ -159,4 +160,7 @@ func (c *Client) StartClientLoop() {
 	log.Infof("action: consulta_ganadores | result: success | cant_ganadores: %d", len(apuestasGanadoras))
 
 	log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
+
+	log.Infof("action: durmiendo para flushear ultimos mensajes | result: success | client_id: %v", c.config.ID)
+	time.Sleep(3 * time.Second)
 }
