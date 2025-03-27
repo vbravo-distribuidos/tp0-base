87c164852e85d2d79ba3d4c3b2a3070a343314da
Victor Bravo <vbravo@fi.uba.ar>
2025-03-26 21:30:04 -0300

    EJ8 - Multiprocessing

---
+++ server/common/concurso.py
@@ -0,0 +1,72 @@
+import socket
+from multiprocessing import Lock, Manager, Pool
+from typing import List
+
+from common.modelo.respuesta import Respuesta
+from common.protocolo.protocolo_agencia import recibir_agencia
+from common.protocolo.protocolo_apuesta import enviar_apuestas, recibir_apuestas
+from common.protocolo.protocolo_respuesta import enviar_respuesta
+from common.utils import Bet, has_won, load_bets, store_bets
+import logging
+
+
+
+def almacenar_apuestas_por_agencia(
+    socket_agencias: List[socket.socket], cantidad_clientes: int
+):
+    with Manager() as manager:
+        lock = manager.Lock()
+        tareas = [(socket, lock) for socket in socket_agencias]
+
+        with Pool(processes=cantidad_clientes) as pool:
+            pool.map(func=almacenar_apuestas, iterable=tareas)
+
+
+def almacenar_apuestas(client_sock: socket.socket, lock):
+    while True:
+        apuestas, cantidad_errores = recibir_apuestas(client_sock)
+        if len(apuestas) == 0:
+            logging.info(
+                "action: apuesta_vacia | result: success | mensaje: termina procesamiento de batches"
+            )
+            break
+
+        with lock:
+            store_bets(apuestas)
+
+        if cantidad_errores == 0:
+            respuesta = Respuesta("OK")
+            logging.info(
+                f"action: apuesta_recibida | result: success | cantidad: {len(apuestas)}"
+            )
+        else:
+            respuesta = Respuesta("ERROR")
+            logging.warning(
+                f"action: apuesta_recibida | result: fail | cantidad: {len(apuestas)}"
+            )
+        enviar_respuesta(client_sock, respuesta)
+
+def filtrar_ganadores(agencia: int, apuestas: List[Bet]) -> List[Bet]:
+    return [
+        apuesta
+        for apuesta in apuestas
+        if apuesta.agency == agencia and has_won(apuesta)
+    ]
+
+
+def responder_ganadores_por_agencia(
+    socket_agencias: List[socket.socket], cantidad_clientes: int
+):
+    with Pool(processes=cantidad_clientes) as pool:
+        pool.map(func=responder_ganadores, iterable=socket_agencias)
+
+
+def responder_ganadores(socket: socket.socket):
+    apuestas = load_bets()
+    agencia = recibir_agencia(socket)
+    logging.info(f"action: recibir_agencias | result: success | agencia: {agencia}")
+    ganadores = filtrar_ganadores(agencia, apuestas)
+    enviar_apuestas(socket, ganadores)
+    logging.info(
+        f"action: apuestas_enviadas | result: success | apuestas: {len(ganadores)}"
+    )
--- server/common/server.py
+++ server/common/server.py
@@ -3,12 +3,10 @@ import signal
 import socket
 from typing import List
 
-from common.modelo.respuesta import Respuesta
-from common.protocolo.protocolo_agencia import recibir_agencia
-from common.protocolo.protocolo_apuesta import (enviar_apuestas,
-                                                recibir_apuestas)
-from common.protocolo.protocolo_respuesta import enviar_respuesta
-from common.utils import Bet, has_won, load_bets, store_bets
+from common.concurso import (
+    almacenar_apuestas_por_agencia,
+    responder_ganadores_por_agencia,
+)
 
 
 class Server:
@@ -37,38 +35,16 @@ class Server:
 
         try:
             socket_agencias = self.aceptar_agencias(self.cantidad_clientes)
-            self.almacenar_apuestas_por_agencia(socket_agencias)
-            self.responder_ganadores_por_agencia(socket_agencias)
+            almacenar_apuestas_por_agencia(socket_agencias, self.cantidad_clientes)
+            logging.info("action: sorteo | result: success")
+            responder_ganadores_por_agencia(socket_agencias, self.cantidad_clientes)
         except OSError as e:
             if self.senial_sigterm_recibida:
-                logging.info("action: finalizacion | result: success | mensaje: termino por sigterm")
+                logging.info(
+                    "action: finalizacion | result: success | mensaje: termino por sigterm"
+                )
             else:
                 logging.error(f"action: finalizacion | result: fail | error: {e}")
-
-    def almacenar_apuestas_por_agencia(self, socket_agencias: List[socket.socket]):
-        for socket_agencia in socket_agencias:
-            self.almacenar_apuestas(socket_agencia)
-
-    def almacenar_apuestas(self, client_sock: socket.socket):
-        while True:
-            apuestas, cantidad_errores = recibir_apuestas(client_sock)
-            if len(apuestas) == 0:
-                logging.info("action: apuesta_vacia | result: success | mensaje: termina procesamiento de batches")
-                break
-
-            store_bets(apuestas)
-
-            if cantidad_errores == 0:
-                respuesta = Respuesta("OK")
-                logging.info(
-                    f"action: apuesta_recibida | result: success | cantidad: {len(apuestas)}"
-                )
-            else:
-                respuesta = Respuesta("ERROR")
-                logging.warning(
-                    f"action: apuesta_recibida | result: fail | cantidad: {len(apuestas)}"
-                )
-            enviar_respuesta(client_sock, respuesta)
 
     def __accept_new_connection(self):
         """
@@ -90,26 +66,3 @@ class Server:
             client_sock = self.__accept_new_connection()
             socket_agencias.append(client_sock)
         return socket_agencias
-
-    def filtrar_ganadores(self, agencia: int, apuestas: List[Bet]) -> List[Bet]:
-        return [
-            apuesta
-            for apuesta in apuestas
-            if apuesta.agency == agencia and has_won(apuesta)
-        ]
-    
-    def responder_ganadores_por_agencia(self, socket_agencias: List[socket.socket]):
-        for socket in socket_agencias:
-            self.responder_ganadores(socket)
-
-    def responder_ganadores(self, socket: socket.socket):
-        apuestas = load_bets()
-        agencia = recibir_agencia(socket)
-        logging.info(
-            f"action: recibir_agencias | result: success | agencia: {agencia}"
-        )
-        ganadores = self.filtrar_ganadores(agencia, apuestas)
-        enviar_apuestas(socket, ganadores)
-        logging.info(
-            f"action: apuestas_enviadas | result: success | apuestas: {len(ganadores)}"
-        )
